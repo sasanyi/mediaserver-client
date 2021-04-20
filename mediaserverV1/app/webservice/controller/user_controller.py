6986b74b8ae698d1eed1a777d4ff81a2602f373b
Savanya Sándor Jószef <savanya.sandor.jozsef@gmail.com>
2021-04-20 23:18:39 +0200

    add findbyid url to user api

--- mediaserverV1/app/webservice/controller/user_controller.py
+++ mediaserverV1/app/webservice/controller/user_controller.py
@@ -10,7 +10,7 @@ _user = UserDto.user
 
 
 @api.route('/')
-class UserList(Resource):
+class UserRoot(Resource):
     @inject
     def __init__(self, user_service: UserService, api):
         super().__init__(api)
@@ -41,3 +41,23 @@ class UserList(Resource):
                 'message': 'User already exists. Please Log in.',
             }
             return response_object, 409
+
+
+@api.route('/<public_id>')
+@api.param('public_id', 'The User identifier')
+@api.response(404, 'User not found.')
+class UserFindById(Resource):
+    @inject
+    def __init__(self, user_service: UserService, api):
+        super().__init__(api)
+        self.user_service = user_service
+
+    @api.doc('get a user')
+    @api.marshal_with(_user)
+    def get(self, public_id):
+        """get a user given its identifier"""
+        user = self.user_service.get_user_by_id(public_id)
+        if not user:
+            api.abort(404)
+        else:
+            return user
